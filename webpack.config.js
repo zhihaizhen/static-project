674010d5f7f3cc3be14302affd346ce64c968296
zhihaizhen <1150305954@qq.com>
2021-12-13 08:09:52 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const webpack = require("webpack");
 const WebpackHtml = require("html-webpack-plugin");
 const WebpackCopy = require("copy-webpack-plugin");
+const pxtorem = require("postcss-pxtorem");
 const Path = require('path');
 
 module.exports = {
     
     entry: {
-        trading: "./static/pages/trading.js",
-        staticJs1: "./static/pages/staticJs1.js",
-        staticJs2: "./static/pages/staticJs2.js",
+        // trading: "./static/pages/trading.js",
+        // staticJs1: "./static/pages/staticJs1.js",
+        // staticJs2: "./static/pages/staticJs2.js",
+        // staticJs3: "./static/pages/staticJs3.js",
+        // staticJs4: "./static/pages/staticJs4.js",
+        staticJs5: "./static/pages/staticJs5.js",
+        staticJs6: "./static/pages/staticJs6.js",
     },
 
     output: {
@@ -28,7 +33,33 @@ module.exports = {
 
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: [
+                {
+                    loader: "style-loader"
+                }, 
+                
+                {
+                    loader: "css-loader"
+                }, 
+                // {
+                //     loader: 'postcss-loader',
+                //     options: {
+                //       postcssOptions: {
+                //         plugins: [
+                //           [
+                //             'postcss-preset-env',
+                //             {
+                //               // 其他选项
+                //             },
+                //           ],
+                //         ],
+                //       },
+                //     },
+                //   },
+                {
+                    loader: "sass-loader"
+                },
+                ]
             },
             {
                 test: /\.(png|jpg|gif)$/,
@@ -72,40 +103,18 @@ module.exports = {
         /**
          * html templates
          */
-
-        // new WebpackHtml({
-
-        //     filename: "templates/trading.html",
-        //     template: "./templates/trading.html",
-        //     chunks: ["trading"],
-        //     hash: true
-        // }),
-
         new WebpackHtml({
 
-            filename: "templates/staticHtml1.html",
-            template: "./templates/staticHtml1.html",
-            chunks: ["staticJs1"],
-            hash: true
-        }),
-
-        new WebpackHtml({
-
-            filename: "templates/staticHtml2.html",
-            template: "./templates/staticHtml2.html",
-            chunks: ["staticJs2"],
+            filename: "index.html",
+            template: "./templates/staticHtml6.html",
+            chunks: ["staticJs6"],
             hash: true
         }),
 
         new WebpackCopy([
 
-            { from: "static/layui", to: "static/layui" },
-            { from: "static/joyin", to: "static/joyin" },
             { from: "static/images", to: "static/images" },
             { from: "static/bootstrap", to: "static/bootstrap" },
-            { from: "static/3rd/axios.min.js", to: "static/3rd/axios.min.js" },
-            { from: "static/type-extension.js", to: "static/type-extension.js" },
-            { from: "static/3rd/split.min.js", to: "static/3rd/split.min.js" },
         ])
     ],
 
